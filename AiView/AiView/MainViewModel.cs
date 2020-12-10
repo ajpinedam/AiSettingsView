ba50f8137ab9a1dc982150a6ce986f8e6a533d71
Andres Pineda <pineda.andres@gmail.com>
2020-12-09 23:22:12 -0500

    Initial Commit

---
+++ AiView/AiView/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmHelpers;
+
+namespace AiView
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private IList<SectionItem> _items;
+        public IList<SectionItem> Items 
+        {
+            get => _items;
+            set => this.SetProperty(ref _items, value);
+        }
+
+        public MainViewModel()
+        {
+            Items = new List<SectionItem> 
+            { 
+                new SectionItem { Title = "Montreal" }, 
+                new SectionItem { Title = "Quebec City" },
+                new SectionItem { Title = "Rosemont" },
+                new SectionItem { Title = "Boucherville" },
+                new SectionItem { Title = "Laval" }
+            };
+        }
+    }
+
+    public class SectionItem
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ AiView/AiView/Settings.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AiView
+{
+    public partial class Settings : ContentView
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        public IList<SectionItem> Items 
+        {
+            get => (IList<SectionItem>)this.GetValue(ItemsProperty);
+            set => this.SetValue(ItemsProperty, value);
+        }
+
+        public static BindableProperty ItemsProperty = BindableProperty.Create(nameof(Items),
+            typeof(IList<SectionItem>), typeof(Settings), null);
+
+            
+    }
+}
